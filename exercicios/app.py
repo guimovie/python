37bb7d39ca6ffecd8e861733b904aa22d348fd41
Guilherme Duarte <gduartedasilva3@gmail.com>
2026-08-14 20:23:24 -0300

    .

--- exercicios/app.py
+++ exercicios/app.py
@@ -1,15 +1,43 @@
-#print('Sabor Express\n')
-print(''' 
-██████████████████████████████████████████████████████████████████████████
-█─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█
-█▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█
-▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀ 
-''')
+import os
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair\n')
+def exibir_nome_do_programa():
+    #print('Sabor Express\n')
+    print(''' 
+    ██████████████████████████████████████████████████████████████████████████
+    █─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█
+    █▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█
+    ▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀ 
+    ''')
 
-opcao_escolhida = input('Escolha uma opção:')
-print(f'Você escolheu a opção {opcao_escolhida}')
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção:'))
+# ou vc pode usar => opcao_escolhida = int(opcao_escolhida)
+#print(f'Você escolheu a opção {opcao_escolhida}')
+
+def finalizar_app():
+    os.system('cls')
+    # os.system('clear') no mac
+    print('Finalizando o app')
+
+def escolher_opcoes():
+    if opcao_escolhida == 1:
+        print('Cadastrar Restaurantes')
+    elif opcao_escolhida == 2:
+        print('Listar Restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar Restaurante')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
---
+++ exercicios/ex-001.py
@@ -0,0 +1,13 @@
+print('Python na Escola de Programação Alura')
+nome = 'Guilherme'
+idade = 23
+print(f'Meu nome é {nome} e tenho {idade} anos\n')
+print('''
+A
+L
+U
+R
+A
+''')
+pi = 3,14159
+print(f'O valor arredondado de PI é: {pi}')
