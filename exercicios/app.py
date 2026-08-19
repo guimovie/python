613777abf4eefae48f2d04279559956ddd22431a
Guilherme Duarte <gduartedasilva3@gmail.com>
2026-08-18 22:37:40 -0300

    Update app.py

--- exercicios/app.py
+++ exercicios/app.py
@@ -1,4 +1,4 @@
-import os
+restaurantes = []
 
 def exibir_nome_do_programa():
     #print('Sabor Express\n')
@@ -15,29 +15,51 @@ def exibir_opcoes():
     print('3. Ativar Restaurante')
     print('4. Sair\n')
 
-opcao_escolhida = int(input('Escolha uma opção:'))
-# ou vc pode usar => opcao_escolhida = int(opcao_escolhida)
-#print(f'Você escolheu a opção {opcao_escolhida}')
 
 def finalizar_app():
-    os.system('cls')
+    print('\033c', end = '')
     # os.system('clear') no mac
     print('Finalizando o app')
 
+def opcao_invalida():
+    print('Opção Invalida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
+def cadastrar_novo_restaurante():
+    print('\033c', end = '')
+    print('CADASTRO DE NOVOS RESTAURANTES\n')
+    nome_do_restaurante = input('Coloque o nome do restaurante q deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso\n')
+    input('Digite qualquer tecla para voltar ao menu principal: ')
+    main()
+
+
 def escolher_opcoes():
-    if opcao_escolhida == 1:
-        print('Cadastrar Restaurantes')
-    elif opcao_escolhida == 2:
-        print('Listar Restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar Restaurante')
-    else:
-        finalizar_app()
+    try:
+        opcao_escolhida = int(input('Escolha uma opção:'))
+        # ou vc pode usar => opcao_escolhida = int(opcao_escolhida)
+        #print(f'Você escolheu a opção {opcao_escolhida}')
+
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            print('Listar Restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar Restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    print('\033c', end = '')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcoes()
-
+    
 if __name__ == '__main__':
     main()
